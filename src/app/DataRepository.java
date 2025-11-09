30b7721e3a24b0ae11a9f8d21cce98e7536f0f46
Volodymyr Dzhevalov <vladtestac@gmail.com>
2025-11-09 11:51:01 +0200

    Add DataRepository to provide data.

---
+++ src/app/DataRepository.java
@@ -0,0 +1,10 @@
+package app;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class DataRepository {
+    public List<String> getData() {
+        return new ArrayList<>(List.of("Alice", "Bob", "Lucy", "Denis", "Tom"));
+    }
+}
